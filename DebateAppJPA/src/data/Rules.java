9384f2de38525891ca9724fec37108bd23ed4208
Teratopia <kylewoodennis@gmail.com>
2017-01-10 13:03:38 -0700

    updated topic and rules

--- DebateAppJPA/src/data/Rules.java
+++ DebateAppJPA/src/data/Rules.java
@@ -30,4 +30,107 @@ public class Rules {
 	private int cpa;
 	private int timeLimit;
 	private boolean oStatements;
+	private boolean references;
+	private int winValue;
+	private boolean publicFlag;
+	private boolean viewersFlag;
+	private boolean commentsView;
+	private boolean privateDebate;
+	
+	public Rules(){}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getApt() {
+		return apt;
+	}
+
+	public void setApt(int apt) {
+		this.apt = apt;
+	}
+
+	public int getCpa() {
+		return cpa;
+	}
+
+	public void setCpa(int cpa) {
+		this.cpa = cpa;
+	}
+
+	public int getTimeLimit() {
+		return timeLimit;
+	}
+
+	public void setTimeLimit(int timeLimit) {
+		this.timeLimit = timeLimit;
+	}
+
+	public boolean isoStatements() {
+		return oStatements;
+	}
+
+	public void setoStatements(boolean oStatements) {
+		this.oStatements = oStatements;
+	}
+
+	public boolean isReferences() {
+		return references;
+	}
+
+	public void setReferences(boolean references) {
+		this.references = references;
+	}
+
+	public int getWinValue() {
+		return winValue;
+	}
+
+	public void setWinValue(int winValue) {
+		this.winValue = winValue;
+	}
+
+	public boolean isPublicFlag() {
+		return publicFlag;
+	}
+
+	public void setPublicFlag(boolean publicFlag) {
+		this.publicFlag = publicFlag;
+	}
+
+	public boolean isViewersFlag() {
+		return viewersFlag;
+	}
+
+	public void setViewersFlag(boolean viewersFlag) {
+		this.viewersFlag = viewersFlag;
+	}
+
+	public boolean isCommentsView() {
+		return commentsView;
+	}
+
+	public void setCommentsView(boolean commentsView) {
+		this.commentsView = commentsView;
+	}
+
+	public boolean isPrivateDebate() {
+		return privateDebate;
+	}
+
+	public void setPrivateDebate(boolean privateDebate) {
+		this.privateDebate = privateDebate;
+	}
+
+	@Override
+	public String toString() {
+		return "Rules [id=" + id + ", apt=" + apt + ", cpa=" + cpa + ", winValue=" + winValue + ", privateDebate="
+				+ privateDebate + "]";
+	}
+	
 }
--- DebateAppJPA/src/data/Topic.java
+++ DebateAppJPA/src/data/Topic.java
@@ -9,6 +9,39 @@ import javax.persistence.Id;
 public class Topic {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	int id;
+	private int id;
+	private String text;
+	private Category category;
+	
+	public Topic(){}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Topic [id=" + id + ", text=" + text + "]";
+	}
+	
 }
