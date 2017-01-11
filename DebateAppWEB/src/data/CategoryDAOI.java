b1fa7c34d35411bf4349e418affe7fc3cbaf62a4
Teratopia <kylewoodennis@gmail.com>
2017-01-11 13:25:52 -0700

    pulling DAOs and DAOIs

--- DebateAppWEB/src/data/CategoryDAOI.java
+++ DebateAppWEB/src/data/CategoryDAOI.java
@@ -1,5 +1,19 @@
 package data;
 
+import java.util.Collection;
+
+import entities.Category;
+
 public interface CategoryDAOI {
+
+	public Collection<Category> index();
+
+	public Category show(int id);
+
+	public Category update(int id, String todoJson);
+
+	public Category create(String todoJson);
+
+	public Category destroy(int id);
 
 }
