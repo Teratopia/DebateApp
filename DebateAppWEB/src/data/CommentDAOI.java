d570fa8bb50e5b15e858f4ef311066c1a9f34e08
Jonathan D Borgia <jonathanborgia@gmail.com>
2017-01-11 12:32:22 -0700

    created CommentDAO

---
+++ DebateAppWEB/src/data/CommentDAOI.java
@@ -0,0 +1,19 @@
+package data;
+
+import java.util.Collection;
+
+import entities.Comment;
+
+public interface CommentDAOI {
+	
+  public Collection<Comment> index();
+
+  public Comment show(int id);
+
+  public Comment update(int id, String commentJson);
+
+  public Comment create(String commentJson);
+
+  public Comment destroy(int id);
+
+}
